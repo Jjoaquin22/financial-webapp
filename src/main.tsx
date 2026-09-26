3d51e17d00aabee7dd5c1ef6255ff14171562846
Gerah Dela Cruz <joaquin.ampusta@gmail.com>
2026-09-26 16:46:09 +0800

    Updated UI

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
+import './finaura-theme.css'
 
 // Configure reCAPTCHA v2 to work with localhost development
 const reCaptchaKey = import.meta.env.VITE_RECAPTCHA_SITE_KEY || ''
